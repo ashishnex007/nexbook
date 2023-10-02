0876363a713c63ee6bd352d086512ef7846318d4
AshishGowtham <122021623+AshishGowtham@users.noreply.github.com>
2023-10-02 15:15:41 +0530

    deployed backend

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -7,7 +7,7 @@ const Signup = () => {
     const handleSubmit = async (e) =>{
         e.preventDefault();
         const {name,email,password} = credentials;
-        const response = await fetch("http://localhost:5000/api/auth/createUser", {
+        const response = await fetch("https://nexbook.onrender.com/api/auth/createUser", {
             method: "POST", 
             headers: {
               "Content-Type": "application/json"
